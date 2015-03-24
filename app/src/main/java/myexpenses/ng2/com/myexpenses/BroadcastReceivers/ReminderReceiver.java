f4990025a893fbb9d07bbcfaadd28e8864888861
Stampoulis Nikolaos <nickstamp93@gmail.com>
2015-03-24 15:58:35 +0200

    ReminderReceiver Clean Code and Debug

--- app/src/main/java/myexpenses/ng2/com/myexpenses/BroadcastReceivers/ReminderReceiver.java
+++ app/src/main/java/myexpenses/ng2/com/myexpenses/BroadcastReceivers/ReminderReceiver.java
@@ -10,8 +10,6 @@ import android.preference.PreferenceManager;
 
 import myexpenses.ng2.com.myexpenses.Activities.OverviewActivity;
 import myexpenses.ng2.com.myexpenses.R;
-import myexpenses.ng2.com.myexpenses.Utils.AlarmService;
-import myexpenses.ng2.com.myexpenses.Utils.SharedPrefsManager;
 
 //broadcast receiver that receives the intents broadcasted by the alarm manager and starts our alarm service
 public class ReminderReceiver extends BroadcastReceiver {
@@ -22,17 +20,9 @@ public class ReminderReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        /*
-        //on receive , create an intent with our service and start it
-        Intent service = new Intent(context , AlarmService.class);
-        context.startService(service);
-*/
-
-        //when service starts , open the preference file
-        SharedPrefsManager prefsManager = new SharedPrefsManager(context);
         //if the daily reminder is enabled then go on to the procedure
-        //if(prefsManager.getPrefsReminder()) {
-        if(PreferenceManager.getDefaultSharedPreferences(context).getBoolean("pref_key_reminder",false)){
+        if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean("pref_key_reminder", false)) {
+
             //get the notification service from the system
             NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
             //create an intent to be  used on click notification
@@ -45,6 +35,8 @@ public class ReminderReceiver extends BroadcastReceiver {
             //this ensures that clicking on the notification opens up the overview activity
             PendingIntent pendingNotificationIntent = PendingIntent.getActivity(context, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
             notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+            //add the notification details
             notification.setLatestEventInfo(context, "Alarm Manager Demo", "notification content", pendingNotificationIntent);
 
             //build the notification and issue it

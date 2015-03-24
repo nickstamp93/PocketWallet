77850cc0313d9ad498e483b65566b67a456ecc5e
Stampoulis Nikolaos <nickstamp93@gmail.com>
2015-03-24 20:34:30 +0200

    PasswordActivity clean code

--- app/src/main/java/myexpenses/ng2/com/myexpenses/Activities/PasswordActivity.java
+++ app/src/main/java/myexpenses/ng2/com/myexpenses/Activities/PasswordActivity.java
@@ -2,12 +2,8 @@ package myexpenses.ng2.com.myexpenses.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +17,7 @@ import myexpenses.ng2.com.myexpenses.Utils.Themer;
 public class PasswordActivity extends Activity {
 
     //number buttons
-    private Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bOk;
+    private Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bOk;
     //password edit text
     private EditText etPassword;
     //shared prefs manager
@@ -30,7 +26,9 @@ public class PasswordActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Themer.setThemeToActivity(this);
+
         setContentView(R.layout.activity_password);
 
         //init manager
@@ -45,7 +43,7 @@ public class PasswordActivity extends Activity {
     }
 
     //init UI
-    private void initUI(){
+    private void initUI() {
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
@@ -62,7 +60,7 @@ public class PasswordActivity extends Activity {
     }
 
     //init listeners
-    private void initListeners(){
+    private void initListeners() {
         b1.setOnClickListener(listener);
         b2.setOnClickListener(listener);
         b3.setOnClickListener(listener);
@@ -84,26 +82,24 @@ public class PasswordActivity extends Activity {
             String text = etPassword.getText().toString();
 
             //if Ok was clicked
-            if(v.getId() == R.id.bOk){
+            if (v.getId() == R.id.bOk) {
                 //check if password is right
                 //if(manager.getPrefsPassword().equals(text)){
-                  if(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("pref_key_password_value" , "").equals(text)){
+                if (PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("pref_key_password_value", "").equals(text)) {
                     //correct password
                     //launch overview activity
-                    Intent intent = new Intent(getApplicationContext() , OverviewActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), OverviewActivity.class);
                     startActivity(intent);
                     //and destroy this one
                     finish();
-                }else{
+                } else {
                     //wrong password
                     //warn the user
-                    Toast.makeText(getApplicationContext() , "Wrong Password" , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Wrong Password", Toast.LENGTH_SHORT).show();
                     //clear the password edit text
                     etPassword.setText("");
                 }
-            }else {
-                //else a number was clicked
-
+            } else {
                 //append the clicked number to the current password
                 text += ((Button) v).getText().toString();
                 etPassword.setText(text);
